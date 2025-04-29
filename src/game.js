d834ac43e59197335c3a91849355edb4b574497f
Vlad <zuzzer.rr@mail.ru>
2025-04-29 08:51:17 +0700

    Implemented getWinner() and snippet tracking. All tests successful

--- src/game.js
+++ src/game.js
@@ -5,6 +5,8 @@ class Game {
     this.player1Score = 0;
     this.player2Score = 0;
     this.currentPlayer = player1;
+    this.snippets = [1, 2, 3];
+    this.currentSnippetIndex = 0;
     }
 
     switchTurn() {
@@ -18,5 +20,18 @@ class Game {
           this.player2Score++;
         }
     }
+
+    getWinner() {
+        if (this.currentSnippetIndex >= this.snippets.length) {
+          if (this.player1Score > this.player2Score) {
+            return this.player1;
+          } else if (this.player2Score > this.player1Score) {
+            return this.player2;
+          } else {
+            return null; // It's a tie
+          }
+        }
+        return null; // Game is not over yet
+      }
 }
   module.exports = Game;
