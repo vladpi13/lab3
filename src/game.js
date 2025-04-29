f7db861ee3f6ad48349b53b87aba3b4899f8e11e
Vlad <zuzzer.rr@mail.ru>
2025-04-29 08:45:30 +0700

    Implemented point awarding. All tests successful

--- src/game.js
+++ src/game.js
@@ -6,9 +6,17 @@ class Game {
     this.player2Score = 0;
     this.currentPlayer = player1;
     }
-    
+
     switchTurn() {
         this.currentPlayer = (this.currentPlayer === this.player1) ? this.player2 : this.player1;
       }
+
+      awardPoint() {
+        if (this.currentPlayer === this.player1) {
+          this.player1Score++;
+        } else {
+          this.player2Score++;
+        }
+    }
 }
   module.exports = Game;
