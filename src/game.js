3bb63fa1f13fcd44f886c32e14984fe3803b0905
Vlad <zuzzer.rr@mail.ru>
2025-04-29 09:16:51 +0700

    Implemented: increase snippet index after correct answer

--- src/game.js
+++ src/game.js
@@ -1,53 +1,55 @@
 const codes = require('./codes');
 
 class Game {
-    constructor(player1, player2) {
-        this.player1 = player1;
+  constructor(player1, player2) {
+    this.player1 = player1;
     this.player2 = player2;
     this.player1Score = 0;
     this.player2Score = 0;
     this.currentPlayer = player1;
     this.snippets = codes;
     this.currentSnippetIndex = 0;
+  }
+
+  switchTurn() {
+    this.currentPlayer = (this.currentPlayer === this.player1) ? this.player2 : this.player1;
+  }
+
+  awardPoint() {
+    if (this.currentPlayer === this.player1) {
+      this.player1Score++;
+    } else {
+      this.player2Score++;
     }
+  }
 
-    switchTurn() {
-        this.currentPlayer = (this.currentPlayer === this.player1) ? this.player2 : this.player1;
+  getWinner() {
+    if (this.currentSnippetIndex >= this.snippets.length) {
+      if (this.player1Score > this.player2Score) {
+        return this.player1;
+      } else if (this.player2Score > this.player1Score) {
+        return this.player2;
+      } else {
+        return null;
       }
-
-      awardPoint() {
-        if (this.currentPlayer === this.player1) {
-          this.player1Score++;
-        } else {
-          this.player2Score++;
-        }
     }
+    return null;
+  }
 
-    getWinner() {
-        if (this.currentSnippetIndex >= this.snippets.length) {
-          if (this.player1Score > this.player2Score) {
-            return this.player1;
-          } else if (this.player2Score > this.player1Score) {
-            return this.player2;
-          } else {
-            return null; 
-          }
-        }
-        return null; 
-      }
+  getCurrentSnippet() {
+    return this.snippets[this.currentSnippetIndex];
+  }
 
-      getCurrentSnippet() {
-        return this.snippets[this.currentSnippetIndex];
-      }
-
-      evaluateGuess(lineNumber, description) {
-        const snippet = this.getCurrentSnippet();
-        const isCorrect = snippet.correctLine === lineNumber && snippet.correctDescription === description;
-        if (isCorrect) {
-            this.awardPoint()
-            this.switchTurn();
-        }
-        return { isCorrect: isCorrect };
-      }
+  evaluateGuess(lineNumber, description) {
+    const snippet = this.getCurrentSnippet();
+    const isCorrect = snippet.correctLine === lineNumber && snippet.correctDescription === description;
+    if (isCorrect) {
+      this.awardPoint()
+      this.switchTurn();
+      this.currentSnippetIndex++;
+    }
+    return { isCorrect: isCorrect };
+  }
 }
-  module.exports = Game;
+
+module.exports = Game;
