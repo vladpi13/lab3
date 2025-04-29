7ddd819f02a10308a1d591a27fcd0728f6f4d355
Vlad <zuzzer.rr@mail.ru>
2025-04-29 08:42:05 +0700

    Implemented score initialization. All tests successful

--- src/game.js
+++ src/game.js
@@ -2,6 +2,8 @@ class Game {
     constructor(player1, player2) {
         this.player1 = player1;
     this.player2 = player2;
+    this.player1Score = 0;
+    this.player2Score = 0;
     }
 }
   module.exports = Game;
