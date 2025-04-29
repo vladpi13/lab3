4479aaec5669753659de3fc34ca69ee0599d0162
Vlad <zuzzer.rr@mail.ru>
2025-04-29 20:08:36 +0700

    fix index.html

--- src/codes.js
+++ src/codes.js
@@ -1,8 +1,14 @@
 const codes = [
     {
-        code: "function add(a, b) {\n  return a +b\n}",
+        code: "function square(a) {\n  return a* a;\n}",
         correctLine: 2,
-        correctDescription: "Отсутствует пробел после +"
+        correctDescription: "Отсутствует пробел после *"
+    },
+    {
+        code: "function multiply(a, b) {\n  retun a * b;\n}",
+        correctLine: 2,
+        correctDescription: "Опечатка в слове return"
+
     },
     {
         code: "function subtract(a, b) {\n  return a -b;\n}",
@@ -10,19 +16,14 @@ const codes = [
         correctDescription: "Отсутствует пробел после -"
     },
     {
-        code: "function multiply(a, b) {\n  retun a * b;\n}",
-        correctLine: 2,
-        correctDescription: "Опечатка в слове return"
-    },
-    {
         code: "function divide(a, b) {\n  if (b = 0) {\n    return null;\n  }\n  return a / b;\n}",
         correctLine: 2,
         correctDescription: "Использовано присваивание вместо сравнения"
     },
     {
-        code: "function square(a) {\n  return a* a;\n}",
+        code: "function greet(name) {\n  return a +b;\n}",
         correctLine: 2,
-        correctDescription: "Отсутствует пробел после *"
+        correctDescription: "Отсутствует пробел после +"
     },
     {
         code: "function power(a, b) {\n  result = 1;\n  for (let i = 0; i <= b; i++) {\n    result *= a;\n  }\n  return result;\n}",
--- src/game.js
+++ src/game.js
@@ -1,62 +1,63 @@
 const codes = require('./codes');
 
 class Game {
-  constructor(player1, player2) {
-    this.player1 = player1;
-    this.player2 = player2;
-    this.player1Score = 0;
-    this.player2Score = 0;
-    this.currentPlayer = player1;
-    this.snippets = codes;
-    this.currentSnippetIndex = 0;
-  }
-
-  switchTurn() {
-    this.currentPlayer = (this.currentPlayer === this.player1) ? this.player2 : this.player1;
-  }
-
-  awardPoint() {
-    if (this.currentPlayer === this.player1) {
-      this.player1Score++;
-    } else {
-      this.player2Score++;
+    constructor(player1, player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+        this.player1Score = 0;
+        this.player2Score = 0;
+        this.currentPlayer = player1;
+        this.snippets = codes;
+        this.currentSnippetIndex = 0;
+        this.attempts = 0;
     }
-  }
 
-  getWinner() {
-    if (this.currentSnippetIndex >= this.snippets.length) {
-      if (this.player1Score > this.player2Score) {
-        return this.player1;
-      } else if (this.player2Score > this.player1Score) {
-        return this.player2;
-      } else {
-        return null;
-      }
+    switchTurn() {
+        this.currentPlayer = (this.currentPlayer === this.player1) ? this.player2 : this.player1;
     }
-    return null;
-  }
 
-  getCurrentSnippet() {
-    return this.snippets[this.currentSnippetIndex];
-  }
-
-  evaluateGuess(lineNumber, description) {
-    const snippet = this.getCurrentSnippet();
-    const isCorrect = snippet.correctLine === lineNumber && snippet.correctDescription === description;
-    if (isCorrect) {
-      this.awardPoint()
-      this.switchTurn();
-      this.currentSnippetIndex++;
-      this.attempts = 0
-    } else {
-        this.attempts++
-        if (this.attempts === 2) {
-            this.switchTurn()
-            this.attempts = 0
+    awardPoint() {
+        if (this.currentPlayer === this.player1) {
+            this.player1Score++;
+        } else {
+            this.player2Score++;
         }
     }
-    return { isCorrect: isCorrect };
-  }
+
+    getWinner() {
+        if (this.currentSnippetIndex >= this.snippets.length) {
+            if (this.player1Score > this.player2Score) {
+                return this.player1;
+            } else if (this.player2Score > this.player1Score) {
+                return this.player2;
+            } else {
+                return null;
+            }
+        }
+        return null;
+    }
+
+    getCurrentSnippet() {
+        return this.snippets[this.currentSnippetIndex];
+    }
+
+    evaluateGuess(lineNumber, description) {
+        const snippet = this.getCurrentSnippet();
+        const isCorrect = snippet.correctLine === lineNumber && snippet.correctDescription === description;
+        if (isCorrect) {
+            this.awardPoint();
+            this.switchTurn();
+            this.currentSnippetIndex++;
+            this.attempts = 0;
+        } else {
+            this.attempts++;
+            if (this.attempts === 2) {
+                this.switchTurn();
+                this.attempts = 0;
+            }
+        }
+        return { isCorrect: isCorrect };
+    }
 }
 
 module.exports = Game;
